5873cf376744a9f5b28ab10d97f7c26ced5e8bb0
vikaschahal2015 <chahalvikas486@gmail.com>
2024-05-28 20:42:22 +0530

    Adding new files

--- main.tf
+++ main.tf
@@ -1,55 +1,44 @@
+# Define provider and region
 provider "aws" {
-  region = "us-east-1"  # Choose your desired region
+  region = "us-east-1"  # Change to your desired region
 }
 
 # Create an IAM role for the Lambda function
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
-# Attach the AWSLambdaBasicExecutionRole managed policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
+# Attach policy to allow basic Lambda execution
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "lambda_basic_execution"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Archive the Lambda function code
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "${path.module}/hello_world.py"
-  output_path = "${path.module}/hello_world.zip"
+  roles      = [aws_iam_role.lambda_role.name]
 }
 
 # Create the Lambda function
-resource "aws_lambda_function" "hello_world" {
-  filename         = data.archive_file.lambda_zip.output_path
-  function_name    = "hello_world_lambda"
-  role             = aws_iam_role.lambda_exec_role.arn
+resource "aws_lambda_function" "hello_world_lambda_12" {
+  function_name    = "hello_world_lambda_test"
   handler          = "hello_world.lambda_handler"
-  runtime          = "python3.9"
-  source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
+  runtime          = "python3.8"  # Change to your desired runtime
+  filename         = "hello_world.zip"  # Provide the path to your Lambda function code
+  source_code_hash = filebase64sha256("hello_world.zip")
+  role             = aws_iam_role.lambda_role.arn
+
+  # (Optional) Environment variables
+  environment {
+    variables = {
+      ENV_VAR_NAME = "ENV_VAR_VALUE"
+    }
+  }
 }
 
-# Allow invocation of the Lambda function using AWS CLI
-resource "aws_lambda_permission" "allow_invoke" {
-  statement_id  = "AllowExecutionFromCLI"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
-  principal     = "lambda.amazonaws.com"
-}
-
-output "lambda_function_name" {
-  value = aws_lambda_function.hello_world.function_name
-}
